0043e5abf9c4351c070064b5b50e3890ca277178
bhargavi8055 <bhargavi8055@gmail.com>
2024-11-15 11:07:03 +0530

    Uncontrolled Components, Accordion Example

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -2,18 +2,19 @@ import { useState } from "react";
 import ItemList from "./ItemList";
 
 const RestaurentCategory = ({data})=>{
-    const [showItems,setShowItems] = useState(false);
+    //Here RestaurentCategory Component manages state by itself it is not controlled by other component - so it is known as UnControlled Components
+    const [showItemsFlag,setShowItemsFlag] = useState(false);
     
     return (
         <div>
-           <div className="shadow-lg w-6/12 bg-gray-50 p-4 mx-auto my-2" onClick={()=>{
-               setShowItems(!showItems)
+           <div className="shadow-lg w-6/12 bg-gray-50 p-4 mx-auto my-2 hover:cursor-pointer" onClick={()=>{
+               setShowItemsFlag(!showItemsFlag)
            }}>
                <div className="flex justify-between">
                     <span className="font-bold text-lg">{data.title} ({data.itemCards.length})</span>
                     <span>⬇</span>
                 </div>
-                {showItems && <ItemList items={data.itemCards} />}
+                {showItemsFlag && <ItemList items={data.itemCards} />}
                
            </div>
          
